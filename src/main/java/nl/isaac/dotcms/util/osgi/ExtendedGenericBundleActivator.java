75267531b4736436a4176c50a44061155e981bc5
Jorith van den Heuvel <jorith.vandenheuvel@isaac.nl>
2019-11-21 08:50:34 +0100

    Fix 'Failed to register servlet MonitoringServlet' error for the dotCMS 4.2.1 version

--- src/main/java/nl/isaac/dotcms/util/osgi/ExtendedGenericBundleActivator.java
+++ src/main/java/nl/isaac/dotcms/util/osgi/ExtendedGenericBundleActivator.java
@@ -44,7 +44,6 @@ import org.quartz.impl.StdSchedulerFactory;
 import com.dotcms.repackage.org.apache.commons.lang.Validate;
 import com.dotcms.repackage.org.apache.logging.log4j.LogManager;
 import com.dotcms.repackage.org.apache.logging.log4j.core.LoggerContext;
-import com.dotcms.rest.WebResource;
 import com.dotcms.rest.config.RestServiceUtil;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.CacheLocator;
@@ -66,6 +65,7 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 	private List<ServiceTracker<ExtHttpService, ExtHttpService>> trackers = new ArrayList<>();
 	private boolean languageVariablesNotAdded = true;
 	private static final String DOTCMS_HOME;
+	private String bundleName;
 
     private Scheduler scheduler;
     private Properties schedulerProperties;
@@ -90,8 +90,9 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 		initializeLoggerContext();
 
 		Bundle bundle = FrameworkUtil.getBundle(this.getClass());
-		String servletPath = "/servlets/monitoring/" + bundle.getHeaders().get("Bundle-Name");
-		addServlet(context, MonitoringServlet.class, servletPath);
+		bundleName = bundle.getHeaders().get("Bundle-Name");
+		String servletPath = "/servlets/monitoring/" + bundleName;
+		addServlet(context, new MonitoringServlet(bundleName), servletPath, false);
 
 	}
 
@@ -122,17 +123,15 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 	protected void addServlet(BundleContext context, final Class<? extends Servlet> clazz, final String path) {
 
 		Validate.notNull(clazz, "Servlet class may not be null");
-		Validate.notEmpty(path, "Servlet path may not be null");
-		Validate.isTrue(path.startsWith("/"), "Servlet path must start with a /");
 
 		final Servlet servlet;
 		try {
 			servlet = clazz.newInstance();
-		} catch (InstantiationException |IllegalAccessException e) {
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
 			throw new RuntimeException(e);
 		}
-
-		Logger.info(this, "Registering Servlet " + servlet.getClass().getSimpleName() + " on /app" + path);
 
 		addServlet(context, servlet, path, false);
 	}
@@ -141,9 +140,20 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 	 * @param handleBundleServices is used to add/remove bundleServices, which are needed for the DispatcherServlet
 	 */
 	private void addServlet(BundleContext context, final Servlet servlet, final String path, final boolean handleBundleServices) {
+		Validate.notEmpty(path, "Servlet path may not be null");
+		Validate.isTrue(path.startsWith("/"), "Servlet path must start with a /");
+
+		Logger.info(this, "Registering Servlet " + servlet.getClass().getSimpleName() + " on /app" + path);
+
 		ServiceTracker<ExtHttpService, ExtHttpService> tracker = new ServiceTracker<ExtHttpService, ExtHttpService>(context, ExtHttpService.class, null) {
 			@Override public ExtHttpService addingService(ServiceReference<ExtHttpService> reference) {
 				ExtHttpService extHttpService = super.addingService(reference);
+
+				try {
+					extHttpService.unregisterServlet(servlet);
+				} catch (Throwable t) {
+					// Do nothing, it was probably not registered
+				}
 
 				try {
 					if(handleBundleServices) {
@@ -152,22 +162,23 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 
 					extHttpService.registerServlet(path, servlet, null, null);
 
-				} catch (Exception e) {
-					throw new RuntimeException("Failed to register servlet " + servlet.getClass().getSimpleName(), e);
+				} catch (Throwable t) {
+					Logger.warn(this, "Failed to register servlet " + servlet.getClass().getSimpleName() + " in bundle " + bundleName, t);
+					throw new RuntimeException(t);
 				}
 				return extHttpService;
 			}
 			@Override public void removedService(ServiceReference<ExtHttpService> reference, ExtHttpService extHttpService) {
-				extHttpService.unregisterServlet(servlet);
-				if(handleBundleServices) {
-					try {
+				try {
+					extHttpService.unregisterServlet(servlet);
+					if (handleBundleServices) {
 						unpublishBundleServices();
-					} catch (Exception e) {
-						Logger.error(this, "Failed to unregister servlet " + servlet.getClass().getSimpleName(), e);
 					}
-				}
 
-				super.removedService(reference, extHttpService);
+					super.removedService(reference, extHttpService);
+				} catch (Throwable t) {
+					Logger.warn(this, "Failed to unregister servlet " + servlet.getClass().getSimpleName() + " in bundle " + bundleName, t);
+				}
 			}
 		};
 
@@ -184,7 +195,9 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 		final Filter filterToRegister;
 		try {
 			filterToRegister = clazz.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
 			throw new RuntimeException(e);
 		}
 
@@ -286,7 +299,9 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 		Logger.info(this, "Registering PreHook " + clazz.getSimpleName());
 		try {
 			super.addPreHook(clazz.newInstance());
-		} catch (InstantiationException | IllegalAccessException e) {
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
 			throw new RuntimeException(e);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
@@ -297,7 +312,9 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 		Logger.info(this, "Registering PostHook " + clazz.getSimpleName());
 		try {
 			super.addPostHook(clazz.newInstance());
-		} catch (InstantiationException | IllegalAccessException e) {
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
 			throw new RuntimeException(e);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
@@ -343,7 +360,7 @@ public abstract class ExtendedGenericBundleActivator extends GenericBundleActiva
 	}
 
 
-	protected void addRestService(BundleContext context, final Class<? extends WebResource> clazz) {
+	protected void addRestService(BundleContext context, final Class<?> clazz) {
 		Logger.info(this, "Registering REST service " + clazz.getSimpleName());
 		ServiceTracker<ExtHttpService, ExtHttpService> tracker = new ServiceTracker<ExtHttpService, ExtHttpService>(context, ExtHttpService.class, null) {
 			@Override public ExtHttpService addingService(ServiceReference<ExtHttpService> reference) {
